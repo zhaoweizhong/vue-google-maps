882d9aff754eaaa44682f03a5e27035dc8beff86
Daniel Sim <daniel.ssq89@gmail.com>
2016-11-12 12:20:31 +0800

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
-/* vim: set softtabstop=2 shiftwidth=2 expandtab : */
-var webpack = require('webpack');
-var path = require('path')
-var _ = require('lodash')
+var webpack = require('webpack')
+var path = require('path');
 
-var baseConfig = {
+module.exports = {
   entry: './src/main.js',
+  output: {
+    path: './dist',
+    publicPath: 'dist/',
+    filename: 'build.js'
+  },
   module: {
     loaders: [
       {
@@ -28,70 +31,23 @@ var baseConfig = {
   babel: {
     presets: ['es2015', 'stage-0'],
     plugins: ['transform-runtime']
-  }
-}; /* baseConfig */
-
-/**
- * Web config uses a global Vue and Lodash object.
- * */
-var webConfig = _.clone(baseConfig);
-webConfig.resolve = {
-    alias: {
-      'vue': path.resolve('./src/stubs/vue'),
-      'lodash': path.resolve('./src/stubs/lodash'),
-    },
-};
-webConfig.output = {
-	path: './dist',
-    filename: "vue-google-maps.js",
-    library: ["VueGoogleMap"],
-    libraryTarget: "umd"
-};
-/**
- *  npm config allows vue-google-maps to be distributed
- *  as an npm package without double-requiring vue
- * */
-var npmConfig = _.clone(baseConfig);
-npmConfig.resolve = {
-    alias: {
-      'vue': path.resolve('./src/stubs-dist/vue'),
-      'lodash': path.resolve('./src/stubs-dist/lodash'),
-      'q': path.resolve('./src/stubs-dist/q'),
-    },
-};
-npmConfig.module.noParse = [
-    /src\/stubs-dist/
-];
-npmConfig.output = {
-	path: './',
-    filename: "index.js",
-    library: ["VueGoogleMap"],
-    libraryTarget: "umd"
-};
-
-module.exports = [
-    webConfig,
-    npmConfig,
-];
-
+  },
+}
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('THIS IS PROD');
-  for (var i=0; i<module.exports.length; i++) {
-      module.exports[i].plugins = [
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: '"production"'
-          }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          compress: {
-            warnings: false
-          }
-        }),
-        new webpack.optimize.OccurenceOrderPlugin()
-      ]
-  }
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
 } else {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = 'source-map'
 }
