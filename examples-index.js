a10a9951441f9f519f9a1b9b6932b6e22a68fa22
daniel_sim <daniel_sim@data.gov.sg>
2017-09-05 22:08:01 +0800

    Updates

---
+++ examples-index.js
@@ -0,0 +1,39 @@
+
+const examples = []
+
+
+import E01Autocomplete_Source from "!!raw-loader!C:\\Users\\Daniel\\Desktop\\vue-google-maps\\examples\\components/01Autocomplete.vue"
+import E01Autocomplete_Module from "C:\\Users\\Daniel\\Desktop\\vue-google-maps\\examples\\components/01Autocomplete.vue"
+
+examples.push({
+  name: "01Autocomplete",
+  module: E01Autocomplete_Module,
+  source: E01Autocomplete_Source,
+  description: E01Autocomplete_Module.description,
+})
+    
+
+import E02LatLngBounds_Source from "!!raw-loader!C:\\Users\\Daniel\\Desktop\\vue-google-maps\\examples\\components/02LatLngBounds.vue"
+import E02LatLngBounds_Module from "C:\\Users\\Daniel\\Desktop\\vue-google-maps\\examples\\components/02LatLngBounds.vue"
+
+examples.push({
+  name: "02LatLngBounds",
+  module: E02LatLngBounds_Module,
+  source: E02LatLngBounds_Source,
+  description: E02LatLngBounds_Module.description,
+})
+    
+
+import E03CurvedLine_Source from "!!raw-loader!C:\\Users\\Daniel\\Desktop\\vue-google-maps\\examples\\components/03CurvedLine.vue"
+import E03CurvedLine_Module from "C:\\Users\\Daniel\\Desktop\\vue-google-maps\\examples\\components/03CurvedLine.vue"
+
+examples.push({
+  name: "03CurvedLine",
+  module: E03CurvedLine_Module,
+  source: E03CurvedLine_Source,
+  description: E03CurvedLine_Module.description,
+})
+    
+
+export default examples
+  
